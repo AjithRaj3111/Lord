0b64361ff193c4f5bd44a5dc7e462e5e5fe7b3ad
AjithRaj3111 <ajithraj311195@gmail.com>
2022-03-20 14:44:39 +0530

    Done

---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,19 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("Saving 10%");
+
+	}
+
+	public void fixed() {
+		System.out.println("Fixed 7%");
+
+	}
+
+	public void deposit() {
+		System.out.println("Deposit 9%");
+
+	}
+
+}
